870b887e5a4e5fa977d96e5527a605ff9224c06d
ricardosikic <ricardo.sikic@gmail.com>
2019-12-29 22:34:07 +0000

    Primer ejercicio de herencias

--- index.js
+++ index.js
@@ -3,13 +3,21 @@
 // Esto se hace a partir de las herencias y la palabra usada en js para definir una herencia es extends, miremos un ejemplo
 
 class Human {
-  constructor(name, age) {
+  constructor(name, age, weight) {
     this.name = name;
     this.age = age;
+    this.weight = weight;
   }
 
   hablar() {
-    console.log(`hola soy ${this.name} y tengo ${this.age}`)
+    console.log(`hello i'm ${this.name}  my age is ${this.age} my weight is ${this.weight} kg \u{1F602}`);
   }
 }
 
+// a continuacion creare una subclase hija que hereda del padre 
+
+class Person extends Human {
+}
+
+let persona_1 = new Person('ricardo', 31, 85);
+console.log(persona_1.hablar());
